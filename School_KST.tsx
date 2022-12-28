86602bb9086b50535a3fc13768552470d425d31b
suri909 <85392818+suri909@users.noreply.github.com>
2022-12-28 10:29:00 +0000

    Deploying to gh-pages from master @ ec498bf3f7a3d388656c465e8dd95a31b5e6615d 🚀

---
+++ School_KST.tsx
@@ -0,0 +1,157 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="School_KST" tilewidth="32" tileheight="32" tilecount="51" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/7.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/8.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/9.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/12.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/13.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/14.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/15_Tür.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/16_Tür.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/17_Tür.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/18_Tür.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/Mauer_01.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/Mauer_02.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/Mauersäule.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/Saeule_weiß_boden.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/Saeule_weiß_normal.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/Wand_weiß_boden.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/Wand_weiß_normal.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Boden/boden.png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Tür - Fenster/Tür_Mauer.png"/>
+ </tile>
+ <tile id="27">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Tür - Fenster/Tür_Mauer_02.png"/>
+ </tile>
+ <tile id="28">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/19.png"/>
+ </tile>
+ <tile id="29">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/27.png"/>
+ </tile>
+ <tile id="30">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/28.png"/>
+ </tile>
+ <tile id="31">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/29.png"/>
+ </tile>
+ <tile id="32">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/30.png"/>
+ </tile>
+ <tile id="33">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/31.png"/>
+ </tile>
+ <tile id="34">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/32.png"/>
+ </tile>
+ <tile id="35">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/21.png"/>
+ </tile>
+ <tile id="36">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/22.png"/>
+ </tile>
+ <tile id="37">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/23.png"/>
+ </tile>
+ <tile id="38">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/24.png"/>
+ </tile>
+ <tile id="39">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/25.png"/>
+ </tile>
+ <tile id="40">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/26.png"/>
+ </tile>
+ <tile id="41">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/20.png"/>
+ </tile>
+ <tile id="42">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/Wand_grau_normal.png"/>
+ </tile>
+ <tile id="43">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Wände/Wand_grau_boden .png"/>
+ </tile>
+ <tile id="46">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Boden/Boden_orange.png"/>
+ </tile>
+ <tile id="47">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Tür - Fenster/Fenster_04.png"/>
+ </tile>
+ <tile id="48">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Tür - Fenster/Fenster_01.png"/>
+ </tile>
+ <tile id="49">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Tür - Fenster/Fenster_02.png"/>
+ </tile>
+ <tile id="50">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Tür - Fenster/Fenster_03.png"/>
+ </tile>
+ <tile id="51">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Tür - Fenster/Fenster_groß_unten.png"/>
+ </tile>
+ <tile id="52">
+  <image width="32" height="32" source="../../../../Desktop/Workadventure_KST/Blöcke/Tür - Fenster/Fenster_groß_oben.png"/>
+ </tile>
+</tileset>
